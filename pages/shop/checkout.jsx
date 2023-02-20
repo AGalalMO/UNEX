5b4340f0060442907c6125dfacc24d1fed3ef4df
AGalalMO <108608751+AGalalMO@users.noreply.github.com>
2023-02-20 21:01:15 +0200

    checkout

--- pages/shop/checkout.jsx
+++ pages/shop/checkout.jsx
@@ -15,12 +15,12 @@ import Layout from "~/src/components/layout";
 
 function Checkout(props) {
   const { t } = useTranslation(["checkout", "common"]);
-  const [submitted,setSubmitted]=useState(false)
+  const [submitted, setSubmitted] = useState(false);
   const router = useRouter();
-  const [fees, setFees] = useState(50)
-  const[required,setRequired]=useState(false)
+  const [fees, setFees] = useState(50);
+  const [required, setRequired] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [cities,setCities]=useState()
+  const [cities, setCities] = useState();
   const cartlist = useSelector((state) => state.cart.cartList);
   const [userData, setUserData] = useState({
     Address: "",
@@ -31,23 +31,21 @@ function Checkout(props) {
   });
 
   const getCities = async () => {
-    const response = await axiosInstance.get(APIS.UTILS.DELIVERY_CITIES)
-    setCities(response.data)
-  }
+    const response = await axiosInstance.get(APIS.UTILS.DELIVERY_CITIES);
+    setCities(response.data);
+  };
   useEffect(() => {
-    getCities()
-  },[])
-  const getCartTotalPrice = (cart,isSub) => {
+    getCities();
+  }, []);
+  const getCartTotalPrice = (cart, isSub) => {
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
       if (cart[i].discountedPrice)
         total += cart[i].discountedPrice * cart[i].count;
       else total += cart[i].price * cart[i].count;
     }
-    if(isSub)
-      return total;
-    else
-      return parseInt(total) + parseInt(cities?.[fees]?.deliveryFees??0);
+    if (isSub) return total;
+    else return parseInt(total) + parseInt(cities?.[fees]?.deliveryFees ?? 0);
   };
 
   const updateUserData = (event) => {
@@ -60,31 +58,27 @@ function Checkout(props) {
     router.push(`/product/${id}`, null, { locale: router.locale });
   };
   const validation = useMemo(() => {
-     if (
-       userData.Address &&
-       userData.City &&
-       userData.Email &&
-       userData.PhoneNumber
-     )
-       return true;
-  
-     else {
-       setRequired(false)
-       return false;
-     }
-  },[userData])
+    if (
+      userData.Address &&
+      userData.City &&
+      userData.Email &&
+      userData.PhoneNumber
+    )
+      return true;
+    else {
+      setRequired(false);
+      return false;
+    }
+  }, [userData]);
 
-
-  const placeOrder = async (e) =>
-  {
+  const placeOrder = async (e) => {
     e.preventDefault();
-    if (!validation)
-    {
-      setRequired(true)
-       return;
-     }
-    
-    setSubmitted(true)
+    if (!validation) {
+      setRequired(true);
+      return;
+    }
+
+    setSubmitted(true);
     await axiosInstance.post(
       APIS.ORDER.PLACE_ORDER,
       { ...userData, PostalCode: "12222", City: cities?.[fees]?.city },
@@ -303,8 +297,6 @@ function Checkout(props) {
                     </div>
                   </aside>
                 </div>
-
-               
               </form>
             </div>
           </div>
